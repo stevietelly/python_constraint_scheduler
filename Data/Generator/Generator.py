63b1b93dcb5a3f9e17a3f8170bf06448f345e7be
Stephen Telian <telianstephen@gmail.com>
2024-03-25 07:23:49 +0300

    Whitespace trailing and class documentation

--- Data/Generator/Generator.py
+++ Data/Generator/Generator.py
@@ -1,21 +1,20 @@
 import random
-import string
-
-from Assets.FileHandling.Read import Read
 from Assets.FileHandling.Write import Write
 from Assets.Functions.Utilities import generate_days
 
 
 class DataGenerator:
-    
-
+    """
+    Data Generator
+    Generating data for testing the different models 
+    """
     def __init__(self, output_file:str = "output.json"):
         """
         A class to generate random data for the sake of testing
         """
         self.output_file = output_file
         self.output = {}
-    
+
     def generateRandomPrefrence(self, objects: list|None = None):
         # TODO:Write the code to generate random preferences
         return objects
@@ -32,7 +31,7 @@ class DataGenerator:
             instructors = [i+1 for i in range(int(total_instructors))]
             result.append({"id": i+1, "preferences": self.generateRandomPrefrence(),"sessions": random.randint(1, 3), "instructors": random.choices(instructors, k=random.randint(1, 3))})
         self.output["units"] = result
-    
+
     def GenerateRooms(self, total):
         total = int(total)
         result = []
@@ -82,8 +81,10 @@ class DataGenerator:
         }
         self.output["configuration"] = config_list
 
-    def GenerateAllInputData(self, instructors:int, groups:int, rooms:int, units:int):
-        self.GenerateConfiguration("", 5, "Monday", "8am", "4pm", 1) # default configuration settings
+    def GenerateAllInputData(self, instructors:int, groups:int, rooms:int, units:int, config: None|dict =None):
+
+        if config is None:self.GenerateConfiguration("", 5, "Monday", "8am", "4pm", 1) # default configuration settings
+        else: self.output["configuration"] = config
         self.GenerateInstructors(instructors)
         self.GenerateRooms(rooms)
         self.GenerateGroups(groups, units)
@@ -92,4 +93,3 @@ class DataGenerator:
     def write(self):
         w = Write('', self.output_file, self.output)
         w.dump()
-
