f443020450fa31c5ba44bea1eea99eb6c6030b9c
Stephen Telian <telianstephen@gmail.com>
2024-04-11 19:13:26 +0300

    new Variable Value

--- Logic/Structure/Variables.py
+++ Logic/Structure/Variables.py
@@ -1,3 +1,5 @@
+from typing import List
+from Assets.Functions.Utilities import return_list_of_daytimes
 from Logic.DateTime.Day import Day
 from Logic.DateTime.DayTime import DayTime
 from Logic.DateTime.Time import Time
@@ -8,43 +10,58 @@ from Objects.Physical.Rooms import Room
 
 
 class Static:
-    def __init__(self, unit, group) -> None:
-        self.group = group
-        self.unit = unit
-
-        self.instructor = None
+    """
+    An Unchanging Variable
+    contains
+    1. Unit
+    2. Group
+    3. Instructor
+    """
+    def __init__(self, unit, group, instructor=None) -> None:
+        self.group: Group = group
+        self.unit: Unit = unit
+        self.instructor: Instructor = instructor
     def __repr__(self):
-        return f'StaticVariable({self.group} taking {self.unit})'
+        return f'StaticVariable with {self.group} taking {self.unit}'
     def __str__(self):
-        return f'StaticVariable({self.group} taking {self.unit})'
-    
+        return f'StaticVariable with {self.group} taking {self.unit}'
+
     def __eq__(self, static):
         if not isinstance(static, Static): return False
         return (self.group.identifier == static.group.identifier) and (self.unit.identifier == static.unit.identifier) 
-    
+
     def __ne__(self, static):
         if not isinstance(static, Static): return False
         return (self.group.identifier != static.group.identifier) and (self.unit.identifier != static.unit.identifier)
 
 
 class Dynamic:
-    def __init__(self, time: Time, day: Day, room: Room) -> None:
-        
-        self.time: Time = time
-        self.Day: Day = day
-        self.room: Room = room
+    """
+    A Changing Variable
+
+    """
+    def __init__(self, times: List[Time], days: List[Day], rooms: List[Room], instructors: List[Instructor]) -> None:
+        self.times: List[Time] = times
+        self.days: List[Day] = days
+        self.daytimes: List[DayTime] = return_list_of_daytimes(self.days, self.times)
+        self.rooms: List[Room] = rooms
+        self.instructors: List[Instructor] = instructors
 
     def __str__(self):
-        return f'DynamicVariable({self.room} at {DayTime(self.day, self.time)})'
-    
-    def __eq__(self, dynamic):
-        if not isinstance(dynamic, Dynamic): return False
-        return (self.time == dynamic.time) and (self.day == dynamic.day) and (self.room == dynamic.room)
-    
-    def __ne__(self, dynamic) -> bool:
-        if not isinstance(dynamic, Dynamic): return False
-        return (self.time != dynamic.time) and (self.day != dynamic.day) and (self.room != dynamic.room)
-    
+        return f'DynamicVariable with {len(self.times)} times, {len(self.days)} days, {len(self.daytimes)} daytimes and {len(self.rooms)} rooms'
 
+
+class Value:
+    """
+    Value for an Assignmesnt
+    """
+    def __init__(self) -> None:
+        self.day = None
+        self.time =  None
+        self.room: Room = None
+        self.instructor: Instructor = None
         
- 
+    
+    def is_filled(self)->bool:
+        
+        return all([self.day is not None, self.time is not None, self.room is not None, self.instructor is not None])
